cb1315cecbf58b2f9b34c560ec254d4bc7400239
krzjoz <krz_joz@windowslive.com>
2020-08-07 16:09:28 +0200

    Changes

---
+++ TimeManagementApplication/src/test/java/com/example/TimeManagmentApp/TimeManagmentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TimeManagmentApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeManagmentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
